46cedc9f4147f2cfd41230ced7969cb456201fa9
arshadbadsha01 <arshad.badsha01@gmail.com>
2025-12-14 00:30:44 +0530

    connected mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,34 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+/* Middlewares */
+app.use(cors());
+app.use(express.json());
+
+/* Test Route */
+app.get("/", (req, res) => {
+  res.send("Backend Server Running");
+});
+
+/* Start Server */
+const start = async () => {
+//   try {
+    await mongoose.connect("mongodb+srv://arshad:1234567890sr@clusterconnect1.jyxhcry.mongodb.net/clusterconnect1");
+);
+    // console.log("✅ MongoDB Connected Successfully");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`🚀 Server running on port ${process.env.PORT}`);
+    });
+//   } catch (error) {
+//     console.error("❌ MongoDB Connection Error:", error.message);
+//   }
+};
+
+start();
