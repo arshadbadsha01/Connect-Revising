883239dd47f13210ceaab94621d5b91622e745d4
arshadbadsha01 <arshad.badsha01@gmail.com>
2026-01-12 16:39:30 +0530

    module 10 completed add don.png in uploads

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,10 +1,37 @@
-import { Router } from "express"; 
+import { Router } from "express";
 
-import { activeCheck } from "../controllers/post.controller.js"; 
+import { activeCheck, createPost } from "../controllers/post.controller.js";
 
-const router = Router(); 
+import multer from "multer";
 
-// router.get("/", activeCheck);
-router.get("/", activeCheck);
+const router = Router();
 
-export default router;
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "uploads/");
+  },
+
+  filename: (req, res, cb) => {
+    cb(null, res.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.route("/").get(activeCheck);
+
+router.route("/post").post(upload.single("media"), createPost);
+
+// router.route("/posts").get(getAllPosts);
+
+// router.route("/delete_post").post(deletePost);
+
+// router.route("/comment_post").post(commentPost);
+
+// router.route("/get_comment_by_post").get(getCommentsByPost);
+
+// router.route("/delete_comment_of_user").post(deleteCommentOfUser);
+
+// router.route("/update_like").post(updateLike);
+
+export default router;
